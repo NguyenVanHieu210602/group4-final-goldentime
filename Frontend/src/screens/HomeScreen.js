188db959e590a629a1febd99bc315580e1150ab4
tnhun <tnhung1802@gmail.com>
2023-03-09 18:31:26 +0700

    Update HomeScreen.js

--- Frontend/src/screens/HomeScreen.js
+++ Frontend/src/screens/HomeScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./../components/Header";
 import Footer from "./../components/Footer";
+import ContactInfo from "../components/homeComponents/ContactInfo";
 
 const HomeScreen = () => {
   window.scrollTo(0, 0);
   return (
     <div>
       <Header />
+      <ContactInfo />
       <Footer />
     </div>
   );
