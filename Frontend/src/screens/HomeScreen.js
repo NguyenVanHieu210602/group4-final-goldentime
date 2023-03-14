6b90ceaa4ebe7787e1a5cdcc79a3d35974da4b7a
tnhun <tnhung1802@gmail.com>
2023-03-14 12:08:00 +0700

    update homepage

--- Frontend/src/screens/HomeScreen.js
+++ Frontend/src/screens/HomeScreen.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./../components/Header";
 import Footer from "./../components/Footer";
 import ContactInfo from "../components/homeComponents/ContactInfo";
+import ShopSection from "../components/homeComponents/ShopSection";
 
 const HomeScreen = () => {
   window.scrollTo(0, 0);
   return (
     <div>
       <Header />
+      <ShopSection />
       <ContactInfo />
       <Footer />
     </div>
