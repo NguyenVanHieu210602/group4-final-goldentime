793248d567240a2a4ce9d28290455f7b2045c3dc
KhanhNguyen2804 <khanhntqbdaf200028@fpt.edu.vn>
2023-03-14 16:37:34 +0700

    Add LoginScreen, UserScreen and ProductScreen

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -1,14 +1,16 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import PrivateRouter from "./PrivateRouter";
 import { listOrders } from "./redux/Actions/OrderActions";
 import { lisProducts } from "./redux/Actions/ProductActions";
 import "./responsive.css";
-
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/productScreen.js";
+import Login from "./screens/LoginScreen";
+import UsersScreen from "./screens/UsersScreen";
 
 function App() {
   const dispatch = useDispatch();
@@ -28,7 +30,10 @@ function App() {
       <Router>
         <Switch>
           <PrivateRouter path="/" component={HomeScreen} exact />
+          <PrivateRouter path="/products" component={ProductScreen} />
           <PrivateRouter path="/product/:id/edit" />
+          <PrivateRouter path="/users" component={UsersScreen} />
+          <Route path="/login" component={Login} />
         </Switch>
       </Router>
     </>
