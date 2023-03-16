b6646fd8d414faa8f1d57d001f7bfce690d21bc6
Hieu <vanhieu210602@gmail.com>
2023-03-16 13:54:52 +0700

    add file code notfound.js and producteditscreen.js

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -11,6 +11,9 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/productScreen.js";
 import Login from "./screens/LoginScreen";
 import UsersScreen from "./screens/UsersScreen";
+import AddProduct from "./screens/AddProduct";
+import NotFound from "./screens/NotFound";
+import ProductEditScreen from "./screens/ProductEditScreen";
 
 function App() {
   const dispatch = useDispatch();
@@ -32,8 +35,14 @@ function App() {
           <PrivateRouter path="/" component={HomeScreen} exact />
           <PrivateRouter path="/products" component={ProductScreen} />
           <PrivateRouter path="/product/:id/edit" />
+          <PrivateRouter path="/addproduct" component={AddProduct} />
           <PrivateRouter path="/users" component={UsersScreen} />
+          <PrivateRouter
+            path="/product/:id/edit"
+            component={ProductEditScreen}
+          />
           <Route path="/login" component={Login} />
+          <PrivateRouter path="*" component={NotFound} />
         </Switch>
       </Router>
     </>
