91c88dbecbb6b6e5a725e62ef6e0e679840eb26d
Hieu <vanhieu210602@gmail.com>
2023-03-28 21:11:53 +0700

    - correct api url

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
+import axios from "axios";
 import store from "./redux/store";
+
+const API_URL =
+  process.env.API_URL || "https://group4-server-goldentime.onrender.com";
+axios.defaults.baseURL = API_URL;
 
 ReactDOM.render(
   <Provider store={store}>
