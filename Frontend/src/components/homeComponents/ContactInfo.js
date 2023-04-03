b9559090dcfaeba3661c03eb31735e76a001ec44
tnhun <tnhung1802@gmail.com>
2023-04-03 15:45:27 +0700

    no message

--- Frontend/src/components/homeComponents/ContactInfo.js
+++ Frontend/src/components/homeComponents/ContactInfo.js
@@ -8,40 +8,40 @@ const ContactInfo = () => {
         <div class="row row-cols-1 row-cols-md-3 g-4">
           <div class="col">
             <div class="card h-100">
-              <img src="images/WATCHNEWS1.jpg" class="card-img-top" alt="..." />
+              <img src="images/w1.png" class="card-img-top" alt="..." />
               <div class="card-body">
-                <h5 class="card-title">How much is Orient rectangular men's watch, where to buy?</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h5 class="card-title">6 Facts You Should Know Before Owning An Orient Watch</h5>
+                <p class="card-text">If you are looking for a high-quality and affordable watch, an Orient Watch will be a perfect choice for you. Before getting an Orient watch on hand, you may need to know a few things more about Orient watches.</p>
               </div>
               <div class="card-footer d-flex justify-content-between ">
                 <small class="text-muted">Last updated 1 month ago</small>
-                <a className="card-link  text-primary " href="#">Đọc tiếp</a>
+                <a className="card-link  text-primary " href="https://bestwatch.sg/blog/watchguide/orient-watch/">Continue reading...</a>
               </div>
             </div>
           </div>
           <div class="col">
             <div class="card h-100">
-              <img src="images/WATCHNEWS2.jpg" class="card-img-top" alt="..." />
+              <img src="images/w2.png" class="card-img-top" alt="..." />
               <div class="card-body">
-                <h5 class="card-title">7 Simplest Ways To Check Genuine Orient Watches</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <h5 class="card-title">Luxury Watches: Top 20 Luxury Watch Brands & Their Collections</h5>
+                <p class="card-text">Discover the world of luxury watches like never before. Explore the top 20 luxury watch brands & each of their collections.</p>
               </div>
               <div class="card-footer d-flex justify-content-between ">
                 <small class="text-muted">Last updated 5 day ago</small>
-                <a className="card-link  text-primary " href="#">Continue reading</a>
+                <a className="card-link  text-primary " href="https://wristadvisor.com/luxury-watches-top-watch-brands/">Continue reading...</a>
               </div>
             </div>
           </div>
           <div class="col">
             <div class="card h-100">
-              <img src="images/WATCHNEWS3.jpg" class="card-img-top" alt="..." />
+              <img src="images/w3.png" class="card-img-top" alt="..." />
               <div class="card-body">
-                <h5 class="card-title">How to wind the Orient mechanical watch properly and enough for newbies</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                <h5 class="card-title">20 Couple Watch Idea to Buy for Yourself or to Gift on Someone's Wedding</h5>
+                <p class="card-text">Couple watch is the best thing to gift to your friend or family. And even a couple can also buy branded couple watch set for their own wedding. To make it easy for you we have made a collection of some of the luxurious couple watches and some from affordable ranges.</p>
               </div>
               <div class="card-footer d-flex justify-content-between ">
                 <small class="text-muted">Last updated 5 day ago</small>
-                <a className="card-link  text-primary " href="#">Continue reading</a>
+                <a className="card-link  text-primary " href="https://www.bookeventz.com/blog/couple-watch-wedding-gift-ideas/">Continue reading...</a>
               </div>
             </div>
           </div>
@@ -87,18 +87,11 @@ const ContactInfo = () => {
           </div>
           <div class="col-lg-5 mb-5 ">
             <h5 class="font-weight-semi-bold mb-3">Get In Touch</h5>
-            <p>Justo sed diam ut sed amet duo amet lorem amet stet sea ipsum, sed duo amet et. Est elitr dolor elitr erat sit sit. Dolor diam et erat clita ipsum justo sed.</p>
+            <p>Our store has only one branch. See our contact information below!</p>
             <div class="d-flex flex-column mb-3 mt-2">
-              <h5 class="font-weight-semi-bold mb-3">Store 1</h5>
-              <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>137 Nguyễn Thị Thập, Thanh Khê Tây, Liên Chiểu, Đà Nẵng 550000, Việt Nam</p>
+              <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>123 Le Duan Street, Hai Chau 2, Hai Chau, Da Nang 550000, Vietnam</p>
               <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>goldentime@gmail.com</p>
-              <p class="mb-2"><i class="fa fa-phone-alt text-primary mr-3"></i>+012 345 67890</p>
-            </div>
-            <div class="d-flex flex-column">
-              <h5 class="font-weight-semi-bold mb-3">Store 2</h5>
-              <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>137 Nguyễn Thị Thập, Thanh Khê Tây, Liên Chiểu, Đà Nẵng 550000, Việt Nam</p>
-              <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>goldentime@gmail.com</p>
-              <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>+012 345 67890</p>
+              <p class="mb-2"><i class="fa fa-phone-alt text-primary mr-3"></i>+84 788 030 999</p>
             </div>
           </div>
         </div>
@@ -108,12 +101,12 @@ const ContactInfo = () => {
             <div class="card mb-3" >
               <div class="row g-0">
                 <div class="col-md-4">
-                  <iframe loading="lazy" title="Đồng Hồ Orient" width="1170" height="658" src="https://www.youtube.com/embed/videoseries?list=PLYNNji2ShyuAgr92B8LPsrNBv3G6zniqI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" data-rocket-lazyload="fitvidscompatible" class="img-fluid rounded-start lazyloaded" data-ll-status="loaded"></iframe>
+                  <iframe loading="lazy" title="Top 10 Most Expensive Watches in the World 2022" width="1170" height="658" src="https://www.youtube.com/embed/rHWZXRctYEY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" data-rocket-lazyload="fitvidscompatible" class="img-fluid rounded-start lazyloaded" data-ll-status="loaded"></iframe>
                 </div>
                 <div class="col-md-8">
                   <div class="card-body">
                     <h5 class="card-title">VIDEO INSTRUCTIONS FOR USE</h5>
-                    <p class="card-text">Bambino watch is a famous product line of Orient Japan with nostalgic design when equipped with curved glass to create beautiful visual effects. Please refer to 5 Bambino curved glass watch products. </p>
+                    <p class="card-text">We are proud to present the official 2022 most expensive watches in the world list, verified by Alux. If you were a billionaire, would you buy one of the most expensive watches in the world?</p>
                   </div>
                 </div>
               </div>
@@ -123,12 +116,12 @@ const ContactInfo = () => {
             <div class="card mb-3" >
               <div class="row g-0">
                 <div class="col-md-4">
-                  <iframe className="img-fluid rounded-start" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3833.802446510125!2d108.16775494970574!3d16.075738143480606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314218e6e07b1c3f%3A0x459e4bf5a2af323e!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYyDEkMOgIE7hurVuZw!5e0!3m2!1svi!2s!4v1677693695249!5m2!1svi!2s" width="600" height="450" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                  <iframe className="img-fluid rounded-start" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3833.905858788116!2d108.21347841468402!3d16.070374288880224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3142183674fc0b27%3A0xe637b39e717fb0f3!2zMTIzIMSQLiBMw6ogRHXhuqluLCBI4bqjaSBDaMOidSAyLCBI4bqjaSBDaMOidSwgxJDDoCBO4bq1bmcgNTUwMDAwLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1680259102133!5m2!1svi!2s" width="600" height="450" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <div class="col-md-8">
                   <div class="card-body">
                     <h5 class="card-title">Store address on the map</h5>
-                    <p class="card-text">137 Nguyễn Thị Thập, Thanh Khê Tây, Liên Chiểu, Đà Nẵng 550000, Việt Nam</p>
+                    <p class="card-text">123 Le Duan Street, Hai Chau 2, Hai Chau, Da Nang 550000, Vietnam</p>
                   </div>
                 </div>
               </div>
@@ -140,8 +133,5 @@ const ContactInfo = () => {
   );
 
 };
-{/*
-<iframe loading="lazy" title="Đồng Hồ Orient" width="1170" height="658" src="https://www.youtube.com/embed/videoseries?list=PLYNNji2ShyuAgr92B8LPsrNBv3G6zniqI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" data-rocket-lazyload="fitvidscompatible" class="lazyloaded" data-ll-status="loaded"></iframe>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3833.802446510125!2d108.16775494970574!3d16.075738143480606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x314218e6e07b1c3f%3A0x459e4bf5a2af323e!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYyDEkMOgIE7hurVuZw!5e0!3m2!1svi!2s!4v1677693695249!5m2!1svi!2s" width="600" height="450" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-*/} export default ContactInfo;
 
+export default ContactInfo;
